4db7dfccc7b16cce119169273e0405c4cf87d00b
ArthPatel0311 <81942849+ArthPatel0311@users.noreply.github.com>
2022-02-19 16:26:23 -0500

    Update Wallet.swift

--- UGAHACKS7/Wallet.swift
+++ UGAHACKS7/Wallet.swift
@@ -9,52 +9,74 @@ import SwiftUI
 
 struct Wallet: View {
     
-    var cards: [Card] = testCards
-    @State private var isCardPressed = false;
+    @State var cards: [Card] = testCards
+    @State private var isCardPresent = false;
     @State var selectedCard: Card?
-    
+    private static let cardOffset: CGFloat = 40.0
+    @State private var isCardPressed = false;
+    @GestureState private var dragState = DragState.inactive
+   
     var body: some View {
-       
+        
         
         VStack {
             TopNavBar()
-           
+            
             Spacer()
             
             
             ZStack {
-                if isCardPressed {
-                ForEach(cards) { card in
-                    CardView(card: card)
-                        .padding(.horizontal,35)
-                        .zIndex(self.zIndex(for: card))
-                        .offset(self.offset(for: card))
-                        .transition((AnyTransition.slide.combined(with: .move(edge:.leading)).combined(with:.opacity)))
-                        .animation(self.transitionAnimation(for: card), value: false)
-                        .gesture(
-                        TapGesture()
-                            .onEnded({ _ in
-                                
-                                withAnimation(.easeOut(duration: 0.15).delay(0.1)) {
-                                    self.isCardPressed.toggle()
-                                    self.selectedCard = self.isCardPressed ? card : nil
-                                }
-                                
-                                
-                                
-                            })
+                if isCardPresent {
+                    ForEach(cards) { card in
+                        CardView(card: card)
+                            .padding(.horizontal,35)
+                            .zIndex(self.zIndex(for: card))
+                            .offset(self.offset(for: card))
+                            .transition((AnyTransition.slide.combined(with: .move(edge:.leading)).combined(with:.opacity)))
+                            .animation(self.transitionAnimation(for: card), value: false)
+                            .gesture(
+                                TapGesture()
+                                    .onEnded({ _ in
+                                        
+                                        withAnimation(.easeOut(duration: 0.15).delay(0.1)) {
+                                            self.isCardPressed.toggle()
+                                            self.selectedCard = self.isCardPressed ? card : nil
+                                        }
+                                    })
+                                    .exclusively(before: LongPressGesture(minimumDuration: 0.05)
+                                                    .sequenced(before: DragGesture())
+                                                    .updating(self.$dragState, body: {(value, state, transition) in
+                                                        
+                                                        switch value {
+                                                        case .first(true):
+                                                            state = .pressing(index: self.index(for: card))
+                                                        case .second(true, let drag) :
+                                                            state = .dragging(index: self.index(for: card), translation: drag? .translation ?? .zero)
+                                                        default:
+                                                            break
+                                                        }
+                                                    })
+                                                    .onEnded({ (value) in
+                                                        guard case .second(true, let drag?) = value else {
+                                                            return
+                                                        }
+                                                        
+                                                        
+                                                        self.rearrangeCards(with: card, dragOffset: drag.translation)
+                                                    })
+                                                )
+                            )
                         
                         
                         
-                        )
-           
-                }
+                        
+                    }
                 }
                 
-                    
+                
             }
             .onAppear {
-                isCardPressed.toggle()
+                isCardPresent.toggle()
             }
             Spacer()
             
@@ -68,9 +90,9 @@ struct Wallet: View {
     
     
     /* Helper functions that help set Z-index of the CardView*/
-
-
-
+    
+    
+    
     private func index(for card: Card) -> Int? {
         
         guard let index = cards.firstIndex(where: {$0.id == card.id}) else {
@@ -79,26 +101,80 @@ struct Wallet: View {
         
         return index
     }
-
-
-
+    
+    
+    
     private func zIndex(for card: Card) -> Double {
         guard let cardIndex = index(for: card) else {
             return 0.0
+        }
+        
+        
+        let defaultZIndex = -Double(cardIndex)
+        
+        if let draggingIndex = dragState.index,
+           cardIndex == draggingIndex {
+            
+            return defaultZIndex + Double(dragState.translation.height/Self.cardOffset)
         }
         
         return -Double(cardIndex)
     }
     
     private func offset(for card: Card) -> CGSize {
+        
+        
+        
+        
         guard let cardIndex = index(for: card) else {
             return CGSize()
         }
         
-        return CGSize(width: 0, height: -50 * CGFloat(cardIndex))
+        if isCardPressed {
+            guard let selectedCard = self.selectedCard,
+                  let selectedCardIndex = index(for: selectedCard) else {
+                      return .zero
+                  }
+            
+            if cardIndex >= selectedCardIndex {
+                return .zero
+            }
+            
+            
+            let offset = CGSize(width: 0, height: 1400)
+            
+            return offset
+            
+            
+            
+        }
+        
+        // Handing drag
+        
+        var pressedOffset = CGSize.zero
+        var dragOffsetY: CGFloat = 0.0
+        
+        if let draggingIndex = dragState.index,
+           cardIndex == draggingIndex {
+            pressedOffset.height = dragState.isPressing ? -20 : 0
+            
+            switch dragState.translation.width {
+            case let width where width < -10: pressedOffset.width = -20
+            case let width where width > 10: pressedOffset.width = 20
+            default: break
+            }
+            dragOffsetY = dragState.translation.height
+        }
+           
+            
+            return CGSize(width: 0 + pressedOffset.width, height: -50 * CGFloat(cardIndex) + pressedOffset.height +  dragOffsetY)
+            
+        
     }
+
+
     
-        /* Card slide animation helper functions*/
+    /* Card slide animation helper functions*/
     
     private func transitionAnimation(for card: Card) -> Animation {
         var delay = 0.0
@@ -109,10 +185,27 @@ struct Wallet: View {
         
         return Animation.spring(response: 0.1, dampingFraction: 0.8, blendDuration: 0.02).delay(delay)
     }
-
     
-
+    
+    
+    private  func rearrangeCards(with card: Card, dragOffset: CGSize) {
+        guard let draggingCardIndex = index(for: card) else {
+            return
+        }
+        
+        var newIndex = draggingCardIndex + Int(-dragOffset.height/Self.cardOffset)
+        newIndex = newIndex >= cards.count ? cards.count - 1 : newIndex
+        newIndex = newIndex < 0 ? 0 : newIndex
+        
+        let removedCard = cards.remove(at: draggingCardIndex)
+        cards.insert(removedCard, at :newIndex)
+        
+        
+    }
+    
+    
 }
+
 
 
 
@@ -125,23 +218,69 @@ struct Wallet: View {
 struct TopNavBar: View {
     
     var body : some View {
-    HStack {
-        Text("Wallet")
-            .font(.system(.largeTitle,design: .rounded))
-            .fontWeight(.heavy)
-        
-        Spacer()
-        
-        Button {
+        HStack {
+            Text("Wallet")
+                .font(.system(.largeTitle,design: .rounded))
+                .fontWeight(.heavy)
             
-        } label : {
-            Image(systemName: "plus.circle.fill")
-                .font(.system(.title))
+            Spacer()
+            
+            Button {
+                
+            } label : {
+                Image(systemName: "plus.circle.fill")
+                    .font(.system(.title))
+            }
+        }
+        .padding(.horizontal)
+        .padding(.top,20)
+        
+    }
+    
+}
+
+
+enum DragState {
+    case inactive
+    case pressing(index: Int? = nil)
+    case dragging(index: Int? = nil, translation: CGSize)
+    
+    
+    var index: Int? {
+        switch self {
+        case .pressing(let index), .dragging(let index, _) :
+            return index
+        case .inactive :
+            return nil
         }
     }
-    .padding(.horizontal)
-    .padding(.top,20)
     
+    var translation: CGSize {
+        switch self {
+        case .inactive, .pressing:
+            return .zero
+        case .dragging(_, let translation):
+            return translation
+        }
     }
+    
+    var isPressing: Bool {
+        switch self {
+        case .pressing, .dragging:
+            return true
+        case .inactive:
+            return false
+        }
+    }
+    
+    var isDragging: Bool {
+        switch self {
+        case .dragging:
+            return true
+        case .inactive, .pressing:
+            return false
+        }
+    }
+    
     
 }

5ef4b00377ec5096cc9cfa1e7f8edadd2da59b35
ArthPatel0311 <81942849+ArthPatel0311@users.noreply.github.com>
2022-02-19 11:16:30 -0500

    Initial Commit

---
+++ UGAHACKS7/UGAHACKS7App.swift
@@ -0,0 +1,17 @@
+//
+//  UGAHACKS7App.swift
+//  UGAHACKS7
+//
+//  Created by Arth Patel on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct UGAHACKS7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
